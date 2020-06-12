4add6cc496a930fe36bcb0403d847d50581035ae
danielmorita <danielkeyti@gmail.com>
2020-06-12 11:12:21 -0300

    Dia 5: Recursos avançados

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -6,6 +6,8 @@ import {LeafletMouseEvent} from 'leaflet';
 import api from '../../services/api';
 import axios from 'axios';
 
+import Dropzone from '../../components/Dropzone';
+
 import './styles.css';
 
 import logo from '../../assets/logo.svg'
@@ -40,6 +42,7 @@ const CreatePoint = () => {
   const [selectedCity, setSelectedCity] = useState('0');
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0]);
+  const [selectedFile, setSelectedFile] = useState<File>();
   
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(postion => {
@@ -124,10 +127,21 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
+    const data = new FormData();
 
-    const data = {
-      name, email, whatsapp, uf, city, latitude, longitude, items
+    data.append('name', name);
+    data.append('email', email);
+    data.append('whatsapp', whatsapp);
+    data.append('uf', uf);
+    data.append('city', city);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('items', items.join(','));
+    
+    if(selectedFile) {
+      data.append('image', selectedFile);
     }
+
 
     await api.post('points', data)
     alert('Ponto de alerta criado!.');
@@ -147,6 +161,8 @@ const CreatePoint = () => {
 
       <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br/> ponto de coleta</h1>
+
+        <Dropzone onFileUploaded={setSelectedFile}/>
 
         <fieldset>
           <legend>
